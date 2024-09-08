b837f7f459426a927b68117bdeb7a76076de215f
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2024-09-08 18:10:49 +0300

    Configure server

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+    fetch(`http://localhost:8000/advertisements`)
+        .then(res => res.json())
+        .then(data => console.log(data));
+
+    return (
+        <div className="App">
+            Hello world
+        </div>
+    );
 }
 
 export default App;
