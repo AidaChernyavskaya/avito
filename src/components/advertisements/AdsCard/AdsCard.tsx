7d83c81a8fd3dc6c7088140b35720ec37181abcb
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2024-09-09 21:40:13 +0300

    Add component AdsCard

---
+++ src/components/advertisements/AdsCard/AdsCard.tsx
@@ -0,0 +1,38 @@
+import React, {FC} from 'react';
+import {Image, Typography} from "antd";
+import ImageIcon from "../../../static/image_icon.svg";
+import {EyeTwoTone, HeartTwoTone} from "@ant-design/icons";
+import {Advertisement} from "../../../types";
+import styles from "./AdsCard.module.css";
+
+interface IAdsCard {
+    advertisement: Advertisement;
+}
+
+const AdsCard: FC<IAdsCard> = ({advertisement}) => {
+    return (
+        <div key={advertisement.id} className={styles.advertisement}>
+            <Image
+                alt={'Фото товара'} title={'Фото товара'}
+                src={advertisement.imageUrl} fallback={ImageIcon}
+                width={200} height={200}
+            />
+            <div className={styles.advertisement_info}>
+                <Typography.Title level={3}>{advertisement.name}</Typography.Title>
+                <Typography.Title level={4}>{advertisement.price} ₽</Typography.Title>
+                <div className={styles.advertisement_stats}>
+                    <div className={styles.advertisement_stats__elem}>
+                        <HeartTwoTone twoToneColor="#eb2f96" className={styles.icon}/>
+                        <Typography.Title level={5}>{advertisement.likes}</Typography.Title>
+                    </div>
+                    <div className={styles.advertisement_stats__elem} >
+                        <EyeTwoTone className={styles.icon}/>
+                        <Typography.Title level={5}>{advertisement.views}</Typography.Title>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AdsCard;
