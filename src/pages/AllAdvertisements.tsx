5285017bf6b2e1e105fa47a904e2f8300eb46ea0
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2024-09-10 13:19:15 +0300

    Add modal box

--- src/pages/AllAdvertisements.tsx
+++ src/pages/AllAdvertisements.tsx
@@ -4,7 +4,7 @@ import AdsService from "../API/AdsService";
 import {Advertisement} from "../types";
 import AdsCard from "../components/advertisements/AdsCard/AdsCard";
 import {Link} from "react-router-dom";
-import {Pagination, Select} from "antd";
+import {Button, Form, Input, Modal, Pagination, Select} from "antd";
 
 const AllAdvertisements = () => {
     const [ads, setAds] = useState<Advertisement[]>([]);
@@ -32,13 +32,55 @@ const AllAdvertisements = () => {
         });
     }
 
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+    const [form] = Form.useForm();
+    const showModal = () => {
+        setIsModalOpen(true);
+    };
+    const handleOk = () => {
+        setIsModalOpen(false);
+    };
+    const handleCancel = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <div data-testid="ads-page">
             <Navbar/>
-            <Select
-                defaultValue={perPage} options={[{value: 3, label: 3},{value: 5, label: 5} ,{value: 10, label: 10}]}
-                onChange={value => setPerPage(value)}
-            />
+            <div className={'options'}>
+                <Select
+                    defaultValue={perPage} options={[{value: 3, label: 3},{value: 5, label: 5} ,{value: 10, label: 10}]}
+                    onChange={value => setPerPage(value)}
+                />
+                <Button type={"primary"} onClick={showModal}>Создать объявление</Button>
+            </div>
+            <Modal title="Создать объявление" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+                <Form form={form} wrapperCol={{offset: 2}} labelCol={{span: 5}} labelAlign={"left"}>
+                    <Form.Item label="Изображение" name="imageUrl">
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item
+                        label="Название" name="name"
+                        rules={[
+                            {required: true, message: 'Пожалуйста, введите название товара!',},
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item label="Описание" name="description">
+                        <Input.TextArea/>
+                    </Form.Item>
+
+                    <Form.Item label="Цена" name="price" rules={[
+                        {required: true, message: 'Пожалуйста, введите стоимость товара!',},
+                    ]}>
+                        <Input />
+                    </Form.Item>
+                </Form>
+            </Modal>
+
             {ads.map(el => (
                 <Link to={`/advertisements/${el.id}`} key={el.id}>
                     <AdsCard advertisement={el}/>
